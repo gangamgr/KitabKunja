a3d6c1c940ed0edf6825c19d223cdd00aebfc512
gangamgr <gangamagar643@gmail.com>
2023-10-01 13:05:18 +0545

    working

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -51,41 +51,41 @@ function Home() {
         </div>
       </div>
 
-      <div className='h-screen w-screen flex flex-col justify-center items-center'>
-        <div className='mt-20 gap-5 flex flex-col'>
-          <div className=' h-80 w-80 bg-green-700 rounded-tl-3xl hover:rounded-tl-5xl '></div>
-
-          <div className='flex flex-col h-80 w-80 bg-green-400 gap-4'>
+      <div className='h-fit w-screen bg-purple-600 flex flex-col justify-center items-center'>
+        <div className=' gap-10 flex flex-row bg-pink-800'>
+          <div className=' bg-green-700 rounded-tl-3xl hover:rounded-tl-5xl h-96 w-96 '></div>
+          <div className='flex flex-col h-96 w-96 bg-green-400 gap-4'>
             <div className=' font-bold text-2xl pt-4 '>BEST IN SELLING SINCE 2020</div>
             <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, minima veniam commodi praesentium beatae earum eveniet quibusdam quae ut saepe voluptatem tempora deleniti.
               Ut culpa, harum id dolorem autem iure!</div>
           </div>
+        </div>
 
-          <div className='h-96 bg-yellow-300 gap-6 flex flex-row justify-center items-center'>
-            <div className='flex flex-col w-72 h-72 bg-blue-400 gap-8'>
-              <div className='font-bold text-2xl '>Best Selling Product</div>
-              <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, minima veniam commodi praesentium beatae earum eveniet quibusdam quae ut saepe voluptatem tempora deleniti.
-                Ut culpa, harum id dolorem autem iure!</div>
-              <div className='h-12 w-24 border-2  flex items-center justify-center bg-green-300'>SEE MORE</div>
-            </div>
-
-            <div className='flex flex-col w-72 h-72 bg-blue-400 gap-8'>
-              <div className='font-bold text-2xl '>Best Selling Product</div>
-              <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, minima veniam commodi praesentium beatae earum eveniet quibusdam quae ut saepe voluptatem tempora deleniti.
-                Ut culpa, harum id dolorem autem iure!</div>
-              <div className='h-12 w-24 border-2  flex items-center justify-center bg-green-300'>SEE MORE</div>
-            </div>
-
-            <div className='flex flex-col w-72 h-72 bg-blue-400 gap-8'>
-              <div className='font-bold text-2xl '>Best Selling Product</div>
-              <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, minima veniam commodi praesentium beatae earum eveniet quibusdam quae ut saepe voluptatem tempora deleniti.
-                Ut culpa, harum id dolorem autem iure!</div>
-              <div className='h-12 w-24 border-2  flex items-center justify-center bg-green-300'>SEE MORE</div>
-            </div>
-
+        <div className=' bg-yellow-300 gap-10 mt-16 flex flex-row justify-center items-center'>
+          <div className='flex flex-col w-96 h-96 bg-blue-400 gap-10'>
+            <div className='font-bold text-2xl '>Best Selling Product</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, minima veniam commodi praesentium beatae earum eveniet quibusdam quae ut saepe voluptatem tempora deleniti.
+              Ut culpa, harum id dolorem autem iure!</div>
+            <div className='h-12 w-24 border-2  flex items-center justify-center bg-green-300'>SEE MORE</div>
           </div>
+
+          <div className='flex flex-col w-96 h-96 bg-blue-400 gap-8'>
+            <div className='font-bold text-2xl '>Best Selling Product</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, minima veniam commodi praesentium beatae earum eveniet quibusdam quae ut saepe voluptatem tempora deleniti.
+              Ut culpa, harum id dolorem autem iure!</div>
+            <div className='h-12 w-24 border-2  flex items-center justify-center bg-green-300'>SEE MORE</div>
+          </div>
+
+          <div className='flex flex-col w-96 h-96 bg-blue-400 gap-8'>
+            <div className='font-bold text-2xl '>Best Selling Product</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, minima veniam commodi praesentium beatae earum eveniet quibusdam quae ut saepe voluptatem tempora deleniti.
+              Ut culpa, harum id dolorem autem iure!</div>
+            <div className='h-12 w-24 border-2  flex items-center justify-center bg-green-300'>SEE MORE</div>
+          </div>
+
         </div>
       </div>
+
 
 
       {/* <div className='h-96 bg-yellow-300 gap-6 flex flex-row justify-center items-center'>
@@ -117,7 +117,7 @@ function Home() {
       </div> */}
 
       <div className='flex flex-col bg-indigo-600'>
-        <div className=' h-24 flex items-center justify-center bg-red-300 font-bold text-3xl '>OUR PRODUCT</div>
+        <div className=' h-32 flex items-center justify-center bg-red-300 font-bold text-3xl '>OUR PRODUCT</div>
 
         <div className='flex flex-col gap-10 px-10 py-10'>
           <div className='flex flex-row px-10 justify-between items-center'>
@@ -194,92 +194,93 @@ function Home() {
 
             </div>
           </div>
-
-          <div className='flex flex-row gap-6 justify-center items-center'>
-
-            <div className='flex flex-col h-96 w-96 bg-green-500'>
-              <div className=' h-96 w-96 '>
-                <div
-                  onClick={handleFlip}
-                  className={`relative card ${activeCard ? 'cardFlip' : ''}`}>
-                  {/* front */}
-                  <div className='front'>
-                    <img src={candle2} alt='image' className='w-96 h-96 absolute bg-cover bg-no-repeat ' />
-                    <div className='relative '><button className='border flex flex-row gap-2 items-center px-5 py-2  text-white hover:bg-black bg-orange-500'> View Detail <BsArrowRightShort /></button></div>
-                  </div>
-
-                  {/* back */}
-                  <div className=' top-0 back absolute w-96 h-96'>
-                    {/* <img src={candle1} alt='image' className='w-72 h-72' /> */}
-                    <div className=' flex flex-col ml-6 h-96 w-96 items-center mt-56 relative'>
-                      <div className=' font-bold text-lg bg-red-500'>Product Name</div>
-                      <div className='font-bold text-base'>RS.250</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-
-            <div className='flex flex-col h-96 w-96 bg-green-500'>
-              <div className=' h-96 w-96 '>
-                <div
-                  onClick={handleFlip}
-                  className={`relative card ${activeCard ? 'cardFlip' : ''}`}>
-                  {/* front */}
-                  <div className='front'>
-                    <img src={candle2} alt='image' className='w-96 h-96 absolute bg-cover bg-no-repeat ' />
-                    <div className='relative '><button className='border flex flex-row gap-2 items-center px-5 py-2  text-white hover:bg-black bg-orange-500'> View Detail <BsArrowRightShort /></button></div>
-                  </div>
-
-                  {/* back */}
-                  <div className=' top-0 back absolute w-96 h-96'>
-                    {/* <img src={candle1} alt='image' className='w-72 h-72' /> */}
-                    <div className=' flex flex-col ml-6 h-96 w-96 items-center mt-56 relative'>
-                      <div className=' font-bold text-lg bg-red-500'>Product Name</div>
-                      <div className='font-bold text-base'>RS.250</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-
-            <div className='flex flex-col h-96 w-96 bg-green-500'>
-              <div className=' h-96 w-96 '>
-                <div
-                  onClick={handleFlip}
-                  className={`relative card ${activeCard ? 'cardFlip' : ''}`}>
-                  {/* front */}
-                  <div className='front'>
-                    <img src={candle2} alt='image' className='w-96 h-96 absolute bg-cover bg-no-repeat ' />
-                    <div className='relative '><button className='border flex flex-row gap-2 items-center px-5 py-2  text-white hover:bg-black bg-orange-500'> View Detail <BsArrowRightShort /></button></div>
-                  </div>
-
-                  {/* back */}
-                  <div className=' top-0 back absolute w-96 h-96'>
-                    {/* <img src={candle1} alt='image' className='w-72 h-72' /> */}
-                    <div className=' flex flex-col ml-6 h-96 w-96 items-center mt-56 relative'>
-                      <div className=' font-bold text-lg bg-red-500'>Product Name</div>
-                      <div className='font-bold text-base'>RS.250</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-
-          </div>
-
         </div>
+
+        <div className='flex flex-col gap-10 px-10 py-10'>
+          <div className='flex flex-row px-10 justify-between items-center'>
+            <div className='flex flex-col h-96 w-96 bg-green-500'>
+              <div className=' h-96 w-96 '>
+                <div
+                  onClick={handleFlip}
+                  className={`relative card ${activeCard ? 'cardFlip' : ''}`}>
+                  {/* front */}
+                  <div className='front'>
+                    <img src={candle2} alt='image' className='w-96 h-96 absolute bg-cover bg-no-repeat ' />
+                    <div className='relative '><button className='border flex flex-row gap-2 items-center px-5 py-2  text-white hover:bg-black bg-orange-500'> View Detail <BsArrowRightShort /></button></div>
+                  </div>
+
+                  {/* back */}
+                  <div className=' top-0 back absolute w-96 h-96'>
+                    {/* <img src={candle1} alt='image' className='w-72 h-72' /> */}
+                    <div className=' flex flex-col ml-6 h-96 w-96 items-center mt-56 relative'>
+                      <div className=' font-bold text-lg bg-red-500'>Product Name</div>
+                      <div className='font-bold text-base'>RS.250</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <div className='flex flex-col h-96 w-96 bg-green-500'>
+              <div className=' h-96 w-96 '>
+                <div
+                  onClick={handleFlip}
+                  className={`relative card ${activeCardTwo ? 'cardFlip' : ''}`}>
+                  {/* front */}
+                  <div className='front'>
+                    <img src={candle2} alt='image' className='w-96 h-96 absolute bg-cover bg-no-repeat ' />
+                    <div className='relative '><button className='border flex flex-row gap-2 items-center px-5 py-2  text-white hover:bg-black bg-orange-500'> View Detail <BsArrowRightShort /></button></div>
+                  </div>
+
+
+                  {/* back */}
+                  <div className=' top-0 back absolute w-96 h-96'>
+                    {/* <img src={candle1} alt='image' className='w-72 h-72' /> */}
+                    <div className=' flex flex-col ml-6 h-96 w-96 items-center mt-56 relative'>
+                      <div className=' font-bold text-lg bg-red-500'>Product Name</div>
+                      <div className='font-bold text-base'>RS.250</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <div className='flex flex-col h-96 w-96 bg-green-500'>
+              <div className=' h-96 w-96 '>
+                <div
+                  onClick={handleFlip}
+                  className={`relative card ${activeCard ? 'cardFlip' : ''}`}>
+                  {/* front */}
+                  <div className='front'>
+                    <img src={candle2} alt='image' className='w-96 h-96 absolute bg-cover bg-no-repeat ' />
+                    <div className='relative '><button className='border flex flex-row gap-2 items-center px-5 py-2  text-white hover:bg-black bg-orange-500'> View Detail <BsArrowRightShort /></button></div>
+                  </div>
+
+                  {/* back */}
+                  <div className=' top-0 back absolute w-96 h-96'>
+                    {/* <img src={candle1} alt='image' className='w-72 h-72' /> */}
+                    <div className=' flex flex-col ml-6 h-96 w-96 items-center mt-56 relative'>
+                      <div className=' font-bold text-lg bg-red-500'>Product Name</div>
+                      <div className='font-bold text-base'>RS.250</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
       </div>
 
       <div className='flex flex-col justify-center items-center'>
-        <div className=' h-24 flex items-end bg-red-800 font-bold text-3xl'>What People Say About Us</div>
-        <div className='text-xl h-14 flex items-center bg-sky-400'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A id sit fugit ipsa harum reiciendis</div>
+        <div className=' h-32 flex items-end bg-red-800 font-bold text-3xl'>What People Say About Us</div>
+        <div className='text-xl h-16 flex items-center bg-sky-400'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A id sit fugit ipsa harum reiciendis</div>
       </div>
 
-      <div className='flex flex-row mt-6 bg-orange-500 gap-5 justify-center'>
+      <div className='flex flex-row mt-10 bg-orange-500 gap-10 justify-center'>
         <div className='h-96 w-96 gap-5 bg-teal-900 flex flex-col justify-center items-center'>
           <div className='w-60'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Harum maiores tenetur laborum quibusdam, eius cum mollitia autem, consectetur, nostrum facilis cupiditate libero quis quod.
